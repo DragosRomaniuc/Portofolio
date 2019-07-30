8294cb561023a5eadc41a766c32c2356076b2e75
Dragos Adrian ROMANIUC <dromaniuc@bitdefender.com>
2019-07-30 23:37:26 +0300

    fixed the **** bug

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 // const middleware = [thunk];
-const allStoreEnhancers = compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const allStoreEnhancers = compose(applyMiddleware(thunk));
 const store = createStore(rootReducer,allStoreEnhancers);
 
 export default store;
